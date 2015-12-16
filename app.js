8e0beac91cef9b31a6c6e748846fc31ac5cf4b63
Guido Faranna <farannaguido@gmail.com>
2015-12-16 20:49:17 -0300

    FiX

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var express = require('express'),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server),
 	nicknames={},
+	admin=['Guido','Joaco'];
 	onoff = 0,
+	admon = 0,
+	flag = 0,
 	jerarquia = 0;
 server.listen(process.env.PORT || 3000, function(){
   console.log('listening on', server.address().port);
@@ -21,16 +24,20 @@ io.sockets.on('connection',function(socket){
 function interna()
 
 {
-	if(socket.jerarquia==0)
+	if(socket.onoff==1)
 	{
+socket.onoff=0;
 	io.sockets.emit('newMessage', {msg:"Admin Off " + socket.nickname, nick:socket.nickname, jerarquia:socket.jerarquia});
 	}
 	else
 	{
-
+socket.onoff=1;
 	io.sockets.emit('newMessage', {msg:"Admin On " + socket.nickname, nick:socket.nickname, jerarquia:socket.jerarquia});
 		}
 }
+	socket.on('admin',function(data,callback){
+if(data)
+		});
 
 	socket.on('newUser',function(data,callback){
 		if(data in nicknames){
@@ -41,45 +48,38 @@ function interna()
 			callback(true);
 			socket.nickname = data;
 			nicknames[socket.nickname] = 1;
+			var nick = data;
+			for(var x = 0; x <= Admin.length; x++)
+		{
+			if(nick == Admin[x])
+			{
+				socket.jerarquia=1;
+			}
+		}
 			socket.jerarquia = 0;
+
 			updatenick();
 
 		}
 
 	});
+
 	socket.on('sendMessage',function(data){
-		if(data=="admp")
-		{
-			if(socket.jerarquia==0)
-			{
-				socket.jerarquia=1;
-			}
-			else
-			{
-				socket.jerarquia=0;
 
-			}
-			
-		}
-
-
-switch(data){
-case "admp":
-interna();
-break;
-case "ban":
-if(socket.jerarquia==1)
-{
-
-
-io.sockets.emit('newMessage',{msg:"colagusano",nick:socket.nickname});
-}
-break;
-default: io.sockets.emit('newMessage', {msg:data, nick:socket.nickname});
-}
-
-
-
+	if(socket.jerarquia == 1 && data == "/gm")
+	{
+		interna();
+	}
+	
+	if(socket.onoff==1)
+	{
+	 io.sockets.emit('admin', {msg:data, nick:socket.nickname});
+	}
+	else
+	{
+	 io.sockets.emit('newMessage', {msg:data, nick:socket.nickname});
+	}
+	
 	});
 
 function updatenick()
